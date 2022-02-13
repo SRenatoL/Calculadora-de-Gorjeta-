1cd985b9ae69636c95ca30dc859980e9f28261f2
SRenatoL <silva.renat136@gmail.com>
2022-02-13 19:20:43 -0300

    Modificações que melhoram a funcionalidade do codigo e mais testes

--- index/scripts/app.js
+++ index/scripts/app.js
@@ -1,19 +1,25 @@
-    let account = document.querySelector('input#account').value
-    let perc = document.querySelector('input#perc').value
-    let people = document.querySelector('input#people').value
 
     var runner = 0
 
     //caso exista um dado sem ser salvo ou apagado o runner impede das operações continuarem 
     function runtime(){
-        if (runner == 0){
+        if (runner != 1){
+
+        let account = parseFloat(document.getElementById('account').value)
+        let perc = parseFloat(document.getElementById('perc').value)
+        let people = parseFloat(document.getElementById('people').value)
+
+        alert(account, perc, people)
             //evita que os inputs estejam vazios ou zerados
-            switch (account, perc, people){
+             switch (account, perc, people){
                 case "":
                     alert('[ERRO] \n Algum campo esta vazio \n Preencha-os antes de prosseguir')
                 break
+                case NaN:
+                    alert('[ERRO] \n Algum campo esta vazio \n Preencha-os antes de prosseguir')
+                break
                 default:
-                    tipsdata()
+                    tipsdata(account, perc, people)
                 break
             }
         }else{
@@ -24,18 +30,16 @@
     //div onde sera enviada as informações dos inputs
     let tips = document.querySelector('div#tipdata')
 
-    //pega os dados enviados pelos inputs, faz os calculos e retorna as informações para o usuario 
-    function tipsdata() {
+    //pega os dados enviados pelos inputs e retorna as informações para o usuario 
+    function tipsdata(account, perc, people) {
         runner = 1
-        var accountform = parseInt(account)
-        var percform = parseInt(perc)
-        var peopleform = parseInt(people)
-        var answer = (accountform * (percform / 100))
-        var division = (answer/peopleform)
+
+        var answer = (account * (perc / 100))
+        var division = (answer/people)
 
         let text = document.createElement('p')
         text.id = 'Infos'
-        text.innerText = `Valor da conta: ${accountform} \n Porcentagem: ${percform} \n Valor da Gorjeta: ${answer} \n Pessoas: ${peopleform} \n Valor por pessoa ${division}`
+        text.innerText = `Valor da conta: ${account} \n Porcentagem: ${perc} \n Valor da Gorjeta: ${answer} \n Pessoas: ${people} \n Valor por pessoa ${division}`
         
         tips.appendChild(text)
     }
