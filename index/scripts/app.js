d859cebe662954610991383ba1136f38dec5e15e
SRenatoL <silva.renat136@gmail.com>
2022-02-14 12:36:52 -0300

    Etapa final no javascript, pequenas auterações no html e testes no css

--- index/scripts/app.js
+++ index/scripts/app.js
@@ -1,45 +1,48 @@
 
-    var runner = 0
+var runner = 0
 
-    //caso exista um dado sem ser salvo ou apagado o runner impede das operações continuarem 
-    function runtime(){
-        if (runner != 1){
+//caso exista um dado sem ser salvo ou apagado o runner impede das operações continuarem 
+function runtime() {
+    if (runner === 0) {
 
         let account = parseFloat(document.getElementById('account').value)
         let perc = parseFloat(document.getElementById('perc').value)
         let people = parseFloat(document.getElementById('people').value)
 
-        alert(account, perc, people)
-            //evita que os inputs estejam vazios ou zerados
-             switch (account, perc, people){
-                case "":
-                    alert('[ERRO] \n Algum campo esta vazio \n Preencha-os antes de prosseguir')
+
+        //evita que os inputs estejam vazios ou zerados
+        switch (account, perc, people) {
+            case "":
+                alert('[ERRO] \n Algum campo esta vazio \n Preencha-os antes de prosseguir')
                 break
-                case NaN:
-                    alert('[ERRO] \n Algum campo esta vazio \n Preencha-os antes de prosseguir')
+            case NaN:
+                alert('[ERRO] \n Algum campo esta vazio \n Preencha-os antes de prosseguir')
                 break
-                default:
-                    tipsdata(account, perc, people)
+            default:
+                tipsdata(account, perc, people)
                 break
-            }
-        }else{
-            alert('[ERRO] Termine a operação atual')
         }
+    } else {
+        alert('[ERRO] Termine a operação atual')
     }
+}
 
-    //div onde sera enviada as informações dos inputs
-    let tips = document.querySelector('div#tipdata')
+//div onde sera enviada as informações dos inputs
+let tips = document.querySelector('div#tipdata')
 
-    //pega os dados enviados pelos inputs e retorna as informações para o usuario 
-    function tipsdata(account, perc, people) {
-        runner = 1
-
-        var answer = (account * (perc / 100))
-        var division = (answer/people)
-
-        let text = document.createElement('p')
-        text.id = 'Infos'
+//pega os dados enviados pelos inputs e retorna as informações para o usuario 
+function tipsdata(account, perc, people) {
+    runner = 1
+    var answer = (account * (perc / 100))
+    var division = (answer / people)
+    let text = document.createElement('p')
+    text.id = 'Infos'
+    if (people > 1) {
         text.innerText = `Valor da conta: ${account} \n Porcentagem: ${perc} \n Valor da Gorjeta: ${answer} \n Pessoas: ${people} \n Valor por pessoa ${division}`
-        
+        tips.appendChild(text)
+    } else {
+        text.innerText = `Valor da conta: ${account} \n Porcentagem: ${perc} \n Valor da Gorjeta: ${answer}`
         tips.appendChild(text)
     }
+
+}
