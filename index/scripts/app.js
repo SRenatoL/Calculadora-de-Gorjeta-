dd3a38d262d81f2d715623b39ef962644903daad
SRenatoL <silva.renat136@gmail.com>
2022-02-14 12:45:22 -0300

    Etapa final no javascript, pequenas auterações no html e testes no css

--- index/scripts/app.js
+++ index/scripts/app.js
@@ -1,13 +1,13 @@
-
+let test = alert(buh)
 var runner = 0
 
 //caso exista um dado sem ser salvo ou apagado o runner impede das operações continuarem 
 function runtime() {
     if (runner === 0) {
 
-        let account = parseFloat(document.getElementById('account').value)
-        let perc = parseFloat(document.getElementById('perc').value)
-        let people = parseFloat(document.getElementById('people').value)
+        var account = parseFloat(document.getElementById('account').value)
+        var perc = parseFloat(document.getElementById('perc').value)
+        var people = parseFloat(document.getElementById('people').value)
 
 
         //evita que os inputs estejam vazios ou zerados
@@ -23,7 +23,13 @@ function runtime() {
                 break
         }
     } else {
-        alert('[ERRO] Termine a operação atual')
+        var newaccount = parseFloat(document.getElementById('account').value)
+        var newperc = parseFloat(document.getElementById('perc').value)
+        var newpeople = parseFloat(document.getElementById('people').value)
+
+        if(newaccount != account || newperc != perc || newpeople != people){
+            test
+        }
     }
 }
 
@@ -32,10 +38,11 @@ let tips = document.querySelector('div#tipdata')
 
 //pega os dados enviados pelos inputs e retorna as informações para o usuario 
 function tipsdata(account, perc, people) {
+
     runner = 1
     var answer = (account * (perc / 100))
     var division = (answer / people)
-    let text = document.createElement('p')
+    var text = document.createElement('p')
     text.id = 'Infos'
     if (people > 1) {
         text.innerText = `Valor da conta: ${account} \n Porcentagem: ${perc} \n Valor da Gorjeta: ${answer} \n Pessoas: ${people} \n Valor por pessoa ${division}`
@@ -44,5 +51,11 @@ function tipsdata(account, perc, people) {
         text.innerText = `Valor da conta: ${account} \n Porcentagem: ${perc} \n Valor da Gorjeta: ${answer}`
         tips.appendChild(text)
     }
+}
+
+function newroll() {
+    let text = document.getElementById('Infos')
+    tips.removeChild(text)
+
 
 }
