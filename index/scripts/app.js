3fe57bc53f65d0eedb2255f2ddb4b1e3b998c24b
SRenatoL <silva.renat136@gmail.com>
2022-02-15 18:35:43 -0300

    Finalização do js

--- index/scripts/app.js
+++ index/scripts/app.js
@@ -5,58 +5,64 @@ var runner = 0
 let tips = document.getElementById('tipdata')
 
 
-
 function runtime() {
 
     var account = parseFloat(document.getElementById('account').value)
     var perc = parseFloat(document.getElementById('perc').value)
     var people = parseFloat(document.getElementById('people').value)
 
+    alert(account)
 
-    if(runner === 0){
-        roll(account, perc, people)
-        runner = 1
-    }else{
-        reroll(account, perc, people)
-        runner =0
+    if (runner === 0) {
+        check(account, perc, people)
+
+    } else {
+        rollagain(account, perc, people)
     }
-}
-
-function reroll(account, perc, people) {
-    let oldtext = document.getElementById('Infos')
-    tips.removeChild(oldtext)
-    roll(account, perc, people)
 }
 //evita que os dados sejam envidas em branco
-function roll(account, perc, people) {
+function check(account, perc, people) {
+    runner = 1
+    let checkaccount = isNaN(account)
 
+    let checkperc = isNaN(perc)
 
-    switch (account, perc, people) {
-        case "":
-            alert('[ERRO] \n Algum campo esta vazio \n Preencha-os antes de prosseguir')
-            break
-        case NaN:
-            alert('[ERRO] \n Algum campo esta vazio \n Preencha-os antes de prosseguir')
-            break
-        default:
-            tipsdata(account, perc, people)
-            break
-    }
+    let checkpeople = isNaN(people)
 
-    //pega os dados enviados pelos inputs e retorna as informações para o usuario 
-    function tipsdata(account, perc, people) {
+    if (checkaccount == false && checkperc == false && checkpeople == false) {
+        tipsdata(account, perc, people)
 
-        
-        var answer = (account * (perc / 100))
-        var division = (answer / people)
-        var text = document.createElement('p')
-        text.id = 'Infos'
-        if (people > 1) {
-            text.innerText = `Valor da conta: ${account} \n Porcentagem: ${perc} \n Valor da Gorjeta: ${answer} \n Pessoas: ${people} \n Valor por pessoa ${division}`
-            tips.appendChild(text)
-        } else {
-            text.innerText = `Valor da conta: ${account} \n Porcentagem: ${perc} \n Valor da Gorjeta: ${answer}`
-            tips.appendChild(text)
-        }
+    } else {
+        alert('[ERRO] \n Algum campo esta vazio \n Preencha-os antes de prosseguir')
     }
 }
+
+
+//pega os dados enviados pelos inputs e retorna as informações para o usuario 
+function tipsdata(account, perc, people) {
+
+
+    var answer = (account * (perc / 100))
+    var division = (answer / people)
+    var text = document.createElement('p')
+    text.id = 'Infos'
+    if (people > 1) {
+        text.innerText = `Valor da conta: ${account} \n Porcentagem: ${perc} \n Valor da Gorjeta: ${answer} \n Pessoas: ${people} \n Valor por pessoa ${division}`
+        tips.appendChild(text)
+    } else {
+        text.innerText = `Valor da conta: ${account} \n Porcentagem: ${perc} \n Valor da Gorjeta: ${answer}`
+        tips.appendChild(text)
+    }
+}
+
+function resetdata() {
+    let erase = document.getElementById('Infos')
+    tips.removeChild(erase)
+    runner = 0
+}
+
+function rollagain(account, perc, people) {
+    resetdata()
+    check(account, perc, people)
+}
+
